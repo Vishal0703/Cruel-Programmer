c198ac8ba99acb33015539a94d96ef918355a85c
Vishal Gupta <vishal7031311@gmail.com>
2021-02-13 18:14:12 +0530

    1. Rotation Level Speed Increase over time 2. Texts Improved

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
     public float rotateSpeed = 5f;
+    float currentSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpeed = rotateSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0f, 0f, rotateSpeed * Time.deltaTime));
+        transform.Rotate(new Vector3(0f, 0f, currentSpeed * Time.deltaTime));
+        currentSpeed += (Time.deltaTime*0.5f);
     }
 }
